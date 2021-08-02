767facf8dab406a0c184e92cd37a41fafb6d2338
FOO JING XIANG <20011262@myrp.edu.sg>
2021-08-02 09:20:45 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/p05_ndpsongs/CustomAdapter.java
@@ -0,0 +1,50 @@
+package com.myapplicationdev.android.p05_ndpsongs;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+
+    Context parent_context;
+    int layout_id;
+    ArrayList<Song> versionList;
+
+    public CustomAdapter(Context context, int resource, ArrayList<Song> objects) {
+        super(context, resource, objects);
+        parent_context = context;
+        layout_id = resource;
+        versionList = objects;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // Obtain the LayoutInflater object
+        LayoutInflater inflater = (LayoutInflater) parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        // "Inflate" the View for each row
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+        // Obtain the UI components and do the necessary binding
+        TextView tvTitle = rowView.findViewById(R.id.textViewTitle);
+        TextView tvSingers = rowView.findViewById(R.id.textViewSingers);
+        TextView tvStars = rowView.findViewById(R.id.textViewStars);
+        TextView tvYear = rowView.findViewById(R.id.textViewYear);
+
+        // Obtain the Android Version information based on the position
+        Song currentVersion = versionList.get(position);
+
+        // Set values to the TextView to display the corresponding information
+        tvTitle.setText(currentVersion.getTitle());
+        tvSingers.setText(currentVersion.getSingers());
+        tvStars.setText(currentVersion.toString());
+        tvYear.setText(Integer.toString(currentVersion.getYearReleased()));
+
+        return rowView;
+    }
+}
